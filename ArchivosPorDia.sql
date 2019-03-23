5798831330323fcf92435a6dbd6d2a486ad54d64
Steven Chavarria <steven.chavarria4@gmail.com>
2019-03-22 20:03:38 -0600

    Llenar la Tabla ArchivosPorDia

---
+++ ArchivosPorDia.sql
@@ -0,0 +1,24 @@
+
+use BDIntegration
+insert into ArchivosPorDia (DiaSemana, NumeroDeArchivo) values (1, 2);
+insert into ArchivosPorDia (DiaSemana, NumeroDeArchivo) values (1, 1);
+insert into ArchivosPorDia (DiaSemana, NumeroDeArchivo) values (1, 3);
+insert into ArchivosPorDia (DiaSemana, NumeroDeArchivo) values (1, 6);
+insert into ArchivosPorDia (DiaSemana, NumeroDeArchivo) values (2, 4);
+insert into ArchivosPorDia (DiaSemana, NumeroDeArchivo) values (2, 5);
+insert into ArchivosPorDia (DiaSemana, NumeroDeArchivo) values (2, 2);
+insert into ArchivosPorDia (DiaSemana, NumeroDeArchivo) values (3, 3);
+insert into ArchivosPorDia (DiaSemana, NumeroDeArchivo) values (3, 1);
+insert into ArchivosPorDia (DiaSemana, NumeroDeArchivo) values (3, 6);
+insert into ArchivosPorDia (DiaSemana, NumeroDeArchivo) values (3, 4);
+insert into ArchivosPorDia (DiaSemana, NumeroDeArchivo) values (3, 2);
+insert into ArchivosPorDia (DiaSemana, NumeroDeArchivo) values (4, 7);
+insert into ArchivosPorDia (DiaSemana, NumeroDeArchivo) values (4, 1);
+insert into ArchivosPorDia (DiaSemana, NumeroDeArchivo) values (4, 5);
+insert into ArchivosPorDia (DiaSemana, NumeroDeArchivo) values (4, 2);
+insert into ArchivosPorDia (DiaSemana, NumeroDeArchivo) values (4, 4);
+insert into ArchivosPorDia (DiaSemana, NumeroDeArchivo) values (4, 6);
+insert into ArchivosPorDia (DiaSemana, NumeroDeArchivo) values (5, 7);
+insert into ArchivosPorDia (DiaSemana, NumeroDeArchivo) values (5, 3);
+insert into ArchivosPorDia (DiaSemana, NumeroDeArchivo) values (5, 5);
+insert into ArchivosPorDia (DiaSemana, NumeroDeArchivo) values (5, 1);
